1df9eb74128c4f0b36a39d514401b1d0b67c6599
Vincent Samy <vincent.samy@aist.go.jp>
2019-10-15 14:01:03 +0900

    Update license to match copyleft.

--- include/wcl/WrenchCone.h
+++ include/wcl/WrenchCone.h
@@ -1,21 +1,19 @@
-/*      File: CWC.h
-*       This file is part of the program WrenchConeLib
-*       Program description : This library implements the Contact Wrench Cone as given [here](https://scaron.info/papers/journal/caron-tro-2016.pdf). It uses cdd for the polyhedron computation and Eigen for the matrix part. Python bindings are also available.
-*       Copyright (C) 2017 -  vsamy (LIRMM). All Right reserved.
-*
-*       This software is free software: you can redistribute it and/or modify
-*       it under the terms of the CeCILL-C license as published by
-*       the CEA CNRS INRIA, either version 1
-*       of the License, or (at your option) any later version.
-*       This software is distributed in the hope that it will be useful,
-*       but WITHOUT ANY WARRANTY without even the implied warranty of
-*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*       CeCILL-C License for more details.
-*
-*       You should have received a copy of the CeCILL-C License
-*       along with this software. If not, it can be found on the official website
-*       of the CeCILL licenses family (http://www.cecill.info/index.en.html).
-*/
+// wrenh-cone-lib: computes a Contact Wrench Cone using polyhedral presentation
+// Copyright (C) 2019 Vincent Samy
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 #include "wcl/api.h"
